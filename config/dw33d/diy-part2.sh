157887447fd561a388bf6417bf2b69170809e669
0xc0000142 <kangxin2017@outlook.com>
2021-01-12 18:31:37 +0800

    new file:   config/dw33d/.config 	renamed:    config/xiaomi/diy-part1.sh -> config/dw33d/diy-part1.sh 	renamed:    config/xiaomi/diy-part2.sh -> config/dw33d/diy-part2.sh 	new file:   config/dw33d/feeds.conf.default 	renamed:    config/xiaomi/settings.ini -> config/dw33d/settings.ini 	deleted:    config/xiaomi/.config 	deleted:    config/xiaomi/diy/package/lean/default-settings/files/openwrt_banner 	deleted:    config/xiaomi/diy/package/lean/default-settings/files/zzz-default-settings 	deleted:    config/xiaomi/diy/target/linux/ramips/image/mt7621.mk 	deleted:    config/xiaomi/feeds.conf.default

--- config/dw33d/diy-part2.sh
+++ config/dw33d/diy-part2.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rm -rf package/ctcgfw/luci-theme-atmaterial
 svn co  https://github.com/liuran001/openwrt-packages/trunk/luci-theme-atmaterial package/ctcgfw/luci-theme-atmaterial
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
